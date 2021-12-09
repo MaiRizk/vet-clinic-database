3e5e7cb5f2ce4607072ac3cde297f83c268c4c45
MaiRizk <mai.riizk@gmail.com>
2021-12-09 22:28:39 +0200

    Create database, table and insert one row in the table created

--- schema.sql
+++ schema.sql
@@ -1,5 +1,3 @@
-/* Database schema to keep the structure of entire database. */
+createdb vet_clinic;
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+CREATE TABLE animals(id INT GENERATED ALWAYS AS IDENTITY, name TEXT NOT NULL, date_of_birth DATE, escape_attempts INT, neutered BOOLEAN, weight_kg DECIMAL);
