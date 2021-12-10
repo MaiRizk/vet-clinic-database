a66c95d96a0f59bcc3130992566ce74c9a8f4652
MaiRizk <mai.riizk@gmail.com>
2021-12-11 00:15:20 +0200

    Add required queries

--- queries.sql
+++ queries.sql
@@ -8,3 +8,34 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = 'yes';
 SELECT * FROM animals EXCEPT SELECT * FROM animals WHERE name = 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+BEGIN;
+RENAME COLUMN species TO unspecified;
+ROLLBACK;
+
+BEGIN;
+RENAME COLUMN species TO digimon WHERE name LIKE "%mon";
+RENAME COLUMN species TO pokemon WHERE species IS NULL;
+COMMIT; 
+
+BEGIN;
+DROP TABLE animals;
+ROLLBACK;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT BIRTH_DATE_DELETION;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO SAVEPOINT BIRTH_DATE_DELETION;
+UPDATE animals SELECT weight_kg = weight_kg *  -1 WHERE weight_kg < 0;
+COMMIT;
+
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT MAX(escape_attempts) FROM animals;
+SELECT * FROM animals WHERE neutered = 'yes';
+SELECT * FROM animals WHERE neutered = 'no';
+SELECT MIN(weight_kg) FROM animals;
+SELECT MAX(weight_kg) FROM animals;
+SELECT AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-01-01'; 
