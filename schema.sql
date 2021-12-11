3b2fe22f7cd2701922006b0f0e10f732dd5b358d
MaiRizk <mai.riizk@gmail.com>
2021-12-11 17:27:09 +0200

    Add changes to schema.sql

--- schema.sql
+++ schema.sql
@@ -1,3 +1,5 @@
 reatedb vet_clinic;
 
-CREATE TABLE animals(id INT GENERATED ALWAYS AS IDENTITY, name TEXT NOT NULL, date_of_birth DATE, escape_attempts INT, neutered BOOLEAN, weight_kg DECIMAL, species TEXT);
+CREATE TABLE animals(id SERIAL PRIMARY kEY, name TEXT NOT NULL, date_of_birth DATE, escape_attempts INT, neutered BOOLEAN, weight_kg DECIMAL, CONSTRAINT species_id FOREIGN KEY(id) REFERENCES species(id), CONSTRAINT owner_id FOREIGN KEY(id) REFERENCES owners(id));
+CREATE TABLE owners(id SERIAL PRIMARY kEY, full_name TEXT, age INT);
+CREATE TABLE species(id SERIAL PRIMARY KEY, name TEXT);
